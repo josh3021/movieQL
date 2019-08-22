5c42065f01038b1dea65f71bc6dc54167dcb92c0
josh3021 <joseonghwan3021@gmail.com>
2019-08-22 18:11:24 +0900

    Extending the Schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,12 @@
+const examplePerson = {
+  name: "John Doe",
+  age: 14,
+  sex: "male"
+}
+
 const resolvers = {
   Query: {
-    name: () => "Awesome Name"
+    person: () => examplePerson
   }
 }
 
